1b2d30ac68cba094bf73887ba522dc7373c8b0ff
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2026-07-10 20:06:54 +0200

    Add eslint-plugin-simple-import-sort with custom groups

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -19,6 +20,29 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1. React
+            ['^react', '^react-dom'],
+            // 2. Internal component imports
+            ['^@/components', '^@/App'],
+            // 3. Internal lib imports
+            ['^@/lib'],
+            // 4. Internal CSS
+            ['^.+\\.css$'],
+            // 5. Internal image imports
+            ['^.+\\.(svg|png|jpe?g|gif|webp|avif)$'],
+          ],
+        },
+      ],
     },
   },
   {
